a0891b31adacf24870b436b7970cbd41c236b695
Abhishek Ranjan <abhishekranjan@Abhisheks-MacBook-Air.local>
2026-05-27 18:32:59 +0530

    chore: remove backend from frontend branch

--- backend/src/routes/attendance.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { AttendanceController } from '../controllers/attendance.controller';
-
-const router = Router();
-
-router.post('/mark-attendance', AttendanceController.markAttendance);
-
-export default router;
